b40751e5909b862ee3473edbfa911b9838902a76
Abdulrahman reda <158822881+AbdulrahmanReda70@users.noreply.github.com>
2024-11-28 16:24:01 +0200

    Update supabase.js

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,2 +1,2 @@
 import { createClient } from '@supabase/supabase-js';
-export const supabase = createClient('https://ciniyknfxnlgaxhyuako.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpbml5a25meG5sZ2F4aHl1YWtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcxNTMwODcsImV4cCI6MjAzMjcyOTA4N30.UQsojjAaEP-9Ab1JWDXh919qU5mUuaAjDvWJ1gCz0eQ');
+export const supabase = createClient('https://uprfurpajbpzdywrkheh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwcmZ1cnBhamJwemR5d3JraGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI4MDM2NzAsImV4cCI6MjA0ODM3OTY3MH0.Ayv5-Qvxr8-sREiR77_IxdRY_b1--J4n841TKdQOE9Q');
