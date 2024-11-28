9646a95d11fb416acfa5b30c970f30b109d080e2
Abdulrahman reda <158822881+AbdulrahmanReda70@users.noreply.github.com>
2024-11-28 16:57:49 +0200

    Update auth.js

--- src/Components/auth.js
+++ src/Components/auth.js
@@ -3,7 +3,7 @@ export function auth(request) {
     const { url } = request;
     console.log('there');
     const pathName = new URL(url).pathname; // the url that the user came from we will pass it as a search param
-    const check_if_token = localStorage.getItem('sb-ciniyknfxnlgaxhyuako-auth-token');
+    const check_if_token = localStorage.getItem('sb-uprfurpajbpzdywrkheh-auth-token');
     // check if there user logged 
     if (check_if_token) {
         return null;
